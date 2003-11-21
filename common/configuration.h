9c0cd687fea14f72149758d9176221d21fd049f6
alriddoch <alriddoch>
2003-11-21 00:49:42 +0000

    2003-11-21  Al Riddoch  <ajr@ecs.soton.ac.uk>
    	* common/configuration.h, common/configuration.cpp:
    	  Add configuration loader code using varconf.

---
+++ common/configuration.h
@@ -0,0 +1,19 @@
+// This file may be redistributed and modified only under the terms of
+// the GNU General Public License (See COPYING for details).
+// Copyright (C) 2001 Alistair Riddoch
+
+#ifndef COMMON_GLOBALS_H
+#define COMMON_GLOBALS_H
+
+#include <string>
+
+namespace varconf {
+  class Config;
+}
+
+extern varconf::Config * global_conf;
+extern std::string skin;
+
+bool loadConfig(int argc, char ** argv, bool server = false);
+
+#endif // COMMON_GLOBALS_H
