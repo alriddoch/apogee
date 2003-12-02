3da4d622e1423418e7a39daa5c1234c1fde4dbe2
alriddoch <alriddoch>
2003-12-02 16:56:32 +0000

    Invoke libtoolize the automake way, and put it in the right place.

--- autogen.sh
+++ autogen.sh
@@ -1,13 +1,5 @@
 #! /bin/sh
 
-echo libtoolize...
-(libtoolize --version) < /dev/null > /dev/null 2>&1 || {
-    echo libtoolize not found
-    exit 1
-}
-
-libtoolize --force --copy
-
 echo aclocal...
 (aclocal --version) < /dev/null > /dev/null 2>&1 || {
     echo aclocal not found
@@ -23,6 +15,14 @@ echo autoheader...
 }    
 
 autoheader
+
+echo libtoolize...
+(libtoolize --version) < /dev/null > /dev/null 2>&1 || {
+    echo libtoolize not found
+    exit 1
+}
+
+libtoolize --automake --force --copy
 
 echo automake...
 (automake --version) < /dev/null > /dev/null 2>&1 || {
